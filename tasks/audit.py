cf58d5634ca76d9de5f18777a49fe20888a3ed68
Janto <janto@team.uber.space>
2019-05-20 14:55:35 +0200

    :fire: rm audit venv task

--- tasks/audit.py
+++ tasks/audit.py
@@ -6,29 +6,14 @@ import invoke
 @invoke.task
 def requirements(ctx, req_file=None):
     """ Print compromised packages in req-file. """
-    req_file = req_file or ctx.audit.req_file
-    cmd = f"safety check --bare --file='{req_file}'"
-    ctx.run(cmd)
-
-
-@invoke.task
-def packages(ctx):
-    """ Print compromised installed packages. """
-    cmd = f"safety check --bare"
-    ctx.run(cmd)
-
-
-@invoke.task
-def all(ctx, req_file=None):
-    """ Print compromised packages. """
-    req_file = req_file or ctx.audit.req_file
-    if pathlib.Path(req_file).exists:
-        requirements(ctx, req_file)
-    packages(ctx)
+    req_file = pathlib.Path(req_file or ctx.audit.req_file).resolve()
+    if req_file.exists:
+        cmd = f"safety check --bare --file='{req_file}'"
+        ctx.run(cmd)
+    else:
+        print(f"[ERROR] no file found at '{req_file}'")
 
 
 ns_audit = invoke.Collection("audit")
 
-ns_audit.add_task(all, default=True)
-ns_audit.add_task(packages)
-ns_audit.add_task(requirements)
+ns_audit.add_task(requirements, default=True)
